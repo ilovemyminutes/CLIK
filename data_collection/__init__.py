50485bddb39e1a59b9e4ce267f524a6c86c06876
iloveslowfood <silkstaff@naver.com>
2022-03-23 15:08:41 +0900

    fix: deactivate __init__ to prevent circular import

--- data_collection/__init__.py
+++ data_collection/__init__.py
@@ -1,3 +1,3 @@
-from .data_refinement import PlanDataRefiner, train_valid_test_split
-from .data_collection import PlanDataCollector
-from .ctr_collection import collect_click_logs, attach_ctr_label
+# from .data_refinement import *
+# from .data_collection import *
+# from .ctr_collection import *
