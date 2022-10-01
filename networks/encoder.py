dc408aabb2966edf456c89165a2f460cb247c06d
louisko-watcha <louis.ko@watcha.com>
2022-10-01 17:01:57 +0900

    refactor: use single quote

--- networks/encoder.py
+++ networks/encoder.py
@@ -1,7 +1,7 @@
+import timm
 import torch
 from torch import nn
 from torchvision import models
-import timm
 from transformers import BertModel, BertConfig
 
 
@@ -16,13 +16,7 @@ class NonLinearMLP(nn.Module):
             - NOTE. It should be the same as PyTorch activation function name. (ex. 'ReLU' for nn.ReLU())
     """
 
-    def __init__(
-        self,
-        input_dim: int,
-        output_dim: int = 128,
-        hidden_dim: int = None,
-        activation: str = "ReLU",
-    ):
+    def __init__(self, input_dim: int, output_dim: int = 128, hidden_dim: int = None, activation: str = 'ReLU'):
         super(NonLinearMLP, self).__init__()
         if hidden_dim is None:
             hidden_dim = input_dim
@@ -50,19 +44,17 @@ class ImageEncoder(nn.Module):
             - False: load just networkm not pretrained weights
     """
 
-    def __init__(
-        self, name: str = "resnet50", output_dim: int = 128, pretrained: bool = True
-    ):
+    def __init__(self, name: str = 'resnet50', output_dim: int = 128, pretrained: bool = True):
         super(ImageEncoder, self).__init__()
 
         # ResNet-variants: NOTE. it depends on torchvision (https://pytorch.org/vision/stable/index.html)
-        if name.startswith("resnet"):
+        if name.startswith('resnet'):
             self.enc = models.__dict__[name](pretrained=pretrained)
             enc_last_layer_input_dim = self.enc.fc.weight.shape[1]
             self.enc.fc = nn.Identity()
 
         # ViT-variants: NOTE. it depends on timm (https://github.com/rwightman/pytorch-image-models)
-        elif name.startswith("vit"):  # vision transformer
+        elif name.startswith('vit'):  # vision transformer
             self.enc = timm.create_model(name, pretrained=pretrained)
             enc_last_layer_input_dim = self.enc.head.weight.shape[1]
             self.enc.head = nn.Identity()
@@ -90,15 +82,10 @@ class TextEncoder(nn.Module):
             - False: load just networkm not pretrained weights
     """
 
-    def __init__(
-        self,
-        name: str = "dsksd/bert-ko-small-minimal",
-        output_dim: int = 128,
-        pretrained: bool = True,
-    ):
+    def __init__(self, name: str = 'dsksd/bert-ko-small-minimal', output_dim: int = 128, pretrained: bool = True):
         super(TextEncoder, self).__init__()
 
-        # BERT-variants: NOTE. it depends on transformers, HuggingFace (https://huggingface.co/docs/transformers/index)
+        # BERT-variants: depends on transformers (https://huggingface.co/docs/transformers/index)
         self.enc = (
             BertModel.from_pretrained(name)
             if pretrained
@@ -108,6 +95,6 @@ class TextEncoder(nn.Module):
         self.proj = NonLinearMLP(enc_last_input_dim, output_dim)
 
     def forward(self, x) -> torch.Tensor:
-        x = self.enc(**x)["pooler_output"]
+        x = self.enc(**x)['pooler_output']
         x = self.proj(x)
         return x
